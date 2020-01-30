441fd4efbec28a8e452975ad858abc623085fba8
Maria <mstrebulyaeva@gmail.com>
2020-01-30 09:40:50 +0300

    Try to unhide button at mobilePhonePage

--- src/test/java/testhomepage/Lab3Steps.java
+++ src/test/java/testhomepage/Lab3Steps.java
@@ -77,6 +77,9 @@ public class Lab3Steps {
         ElectronicaPage electronicaPage= homePage.clickOnElectronicaMenu();
         MobilePhonePage mobilePhonePage=electronicaPage.clickOnMobilePhone();
         mobilePhonePage.checkRealmeBrand();
+        mobilePhonePage.checkXiaomiBrand();
+        mobilePhonePage.sortByPrice();
+        mobilePhonePage.addFirstXiaomiToCompare();
 
 /*
         try {
@@ -95,7 +98,7 @@ public class Lab3Steps {
     public void quitBrowser (String browser) {
         if(driver!=null){
             Log.info("Quit from " + browser);
-            driver.quit();
+//            driver.quit();
         }
     }
 }
--- src/test/java/testhomepage/MobilePhonePage.java
+++ src/test/java/testhomepage/MobilePhonePage.java
@@ -2,11 +2,12 @@ package testhomepage;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.JavascriptExecutor;
+
+import java.util.concurrent.TimeUnit;
 
 public class MobilePhonePage {
     private static Logger Log = LogManager.getLogger(MarketHome.class);
@@ -14,8 +15,23 @@ public class MobilePhonePage {
 
     public MobilePhonePage(WebDriver driver) {this.driver = driver;}
 
-    By loc_realme = By.xpath("//input[@type='checkbox' and @name='Производитель realme']");
-    By loc_xiaomi = By.xpath("//input[@type='checkbox' and @name='Производитель Xiaomi']");
+    By loc_realme = By.xpath("//span[@class='NVoaOvqe58' and contains(text(), 'realme')]");
+    By loc_xiaomi = By.xpath("//span[@class='NVoaOvqe58' and contains(text(), 'Xiaomi')]");
+    By loc_sortPrice = By.xpath("//a[@class = 'link link_theme_major n-filter-sorter__link i-bem link_js_inited' and contains(text(), 'по цене')]");
+    By loc_compareXiaomi = By.cssSelector("div[data-bem*='Смартфон Xiaomi']");
+
+    public void unHideElem (WebDriver driver, WebElement element){
+        String script = "arguments[0].style.opacity=1;"
+                + "arguments[0].style['transform']='translate(0px, 0px) scale(1)';"
+                + "arguments[0].style['MozTransform']='translate(0px, 0px) scale(1)';"
+                + "arguments[0].style['WebkitTransform']='translate(0px, 0px) scale(1)';"
+                + "arguments[0].style['msTransform']='translate(0px, 0px) scale(1)';"
+                + "arguments[0].style['OTransform']='translate(0px, 0px) scale(1)';"
+                + "return true;";
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+        js.executeScript(script, element);
+    }
 
     public void checkRealmeBrand() throws Exception
     {
@@ -24,12 +40,14 @@ public class MobilePhonePage {
             (new WebDriverWait(driver, 4))
                     .until(ExpectedConditions.visibilityOfElementLocated(loc_realme))
                     .click();
+
         } catch (Exception e) {
             Log.error("Error - brand Realme is absent at the page", e);
             throw e;
         }
         Log.info("Filter by Realme brand have been applied");
     }
+
     public void checkXiaomiBrand() throws Exception
     {
         try {
@@ -41,5 +59,42 @@ public class MobilePhonePage {
             throw e;
         }
         Log.info("Filter by Xiaomi brand have been applied");
+    }
+
+    public void sortByPrice() throws Exception
+    {
+        try {
+            Log.info("Try to sort result by price");
+            (new WebDriverWait(driver, 4))
+                    .until(ExpectedConditions.elementToBeClickable(loc_sortPrice)).click();
+        } catch (Exception e) {
+            Log.error("Error with Price filter", e);
+            throw e;
+        }
+        Log.info("Filter by Price have been applied");
+    }
+
+    public void addFirstXiaomiToCompare() throws Exception
+    {
+        driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+        try {
+            Log.info("Try to add first Xiaomi To Compare");
+            WebElement el = driver.findElement(loc_compareXiaomi);
+            unHideElem(driver, el);
+            el.click();
+/*
+            Log.info("Try to add first Xiaomi To Compare");
+            (new WebDriverWait(driver, 4))
+                    .until(ExpectedConditions.elementToBeClickable(loc_compareXiaomi)).click();
+
+*/
+        } catch (Exception e) {
+            Log.error("Error when add first Xiaomi To Compare", e);
+            throw e;
+        }
+        finally {
+            driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        }
+        Log.info("First Xiaomi added to compare successfully");
     }
 }
